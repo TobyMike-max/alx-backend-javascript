0ce223effeaaff92809ff4c846266b949d967483
TobyMike-max <osinubi.michael@gmail.com>
2023-01-04 12:47:10 +0100

    [Create]: File for task

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+	return arr.filter(({ location }) => location === city).
+		map((item) => {
+			const filRec = { ...item };
+
+			const newRec = newGrades.find((stud) => stud.studentId === item.id);
+			if (newRec) filRec.grade = newRec.grade;
+			else filRec.grade = 'N/A';
+			return filRec;
+		})
+}
