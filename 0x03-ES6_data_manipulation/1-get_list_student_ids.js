fc2dcf694581059a1b036d13cf156b1c6c38224c
TobyMike-max <osinubi.michael@gmail.com>
2023-01-04 01:40:26 +0100

    [Modified]: Else statement check

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,3 @@
 export default function getListStudentIds(arr) {
-  if (arr && Array.isArray(arr)) {
-    return arr.map((obj) => obj.id);
-  } else return [];
+  (arr && Array.isArray(arr)) ? return arr.map((obj) => obj.id) : return [];
 }
