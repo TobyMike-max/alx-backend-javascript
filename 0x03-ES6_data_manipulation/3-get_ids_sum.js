1a836b0a666dbe7a68235233bbbcce39b8f184a3
TobyMike-max <osinubi.michael@gmail.com>
2023-01-04 02:01:25 +0100

    [Modified]: Arrow function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,2 @@
-export default function getStudentIdsSum(arr) {
-	return arr.reduce((acc, { id }) => id + acc, 0);
-}
+const getStudentIdsSum = (arr) => arr.reduce((acc, { id }) => id + acc, 0);
+export default getStudentIdsSum;
