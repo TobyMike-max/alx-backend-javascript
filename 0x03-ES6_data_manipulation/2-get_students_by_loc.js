30fee5e219473fdebc2986e71dd8c2ed6c78c4c4
TobyMike-max <osinubi.michael@gmail.com>
2023-01-04 02:07:11 +0100

    [Modified]: Arrow function

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(arr, city) {
-  if (Array.isArray(arr)) {
-    return arr.filter(({ location }) => (location === city));
-  } else return [];
+  return (Array.isArray(arr)) ? arr.filter(({ location }) => (location === city)) : [];
 }
