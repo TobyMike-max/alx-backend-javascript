f3173ca6faf2920d6e39e0fc3b94d9023491915e
TobyMike-max <osinubi.michael@gmail.com>
2023-01-05 14:30:41 +0100

    [Modified]: Changed map creation

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,6 @@
-const groceriesList = () => {
-  return new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
-};
+const groceriesList = () => new Map().set('Apples', 10)
+  .set('Tomatoes', 10)
+  .set('Pasta', 1)
+  .set('Rice', 1)
+  .set('Banana', 5);
 export default groceriesList;
