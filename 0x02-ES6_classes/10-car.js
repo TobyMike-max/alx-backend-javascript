e858bf30b89a338ff0d46dd160af5533ae3af13a
TobyMike-max <osinubi.michael@gmail.com>
2023-01-16 15:41:13 +0100

    [Modified]: Fixed linter error and argument typo

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,11 @@
 export default class Car {
-  constructor(brnad, motor, color) {
+  constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  cloneCar () {
+  cloneCar() {
     const origin = this;
     return Object.assign(Object.create(Object.getPrototypeOf(origin)), {
       _brand: undefined,
