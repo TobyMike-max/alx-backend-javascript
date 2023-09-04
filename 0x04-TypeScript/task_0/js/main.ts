ded7c0af76809d685934b339e2c59796478204b7
TobyMike-max <osinubi.michael@gmail.com>
2023-09-04 17:02:23 +0100

    Modified files, back into ALX

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,4 +19,31 @@ const stud2: Student = {
 	location: 'Canada',
 };
 
-const studentsList = [stud1, stud2];
+const studentsList: Student[] = [stud1, stud2];
+
+function renderTable() {
+	const table = document.createElement('table');
+
+	const headers = ['First Name', 'Location'];
+	const headerRow = table.insertRow();
+	headers.forEach((header) => {
+		const th = document.createElement('th');
+		th.textContent = header;
+		headerRow.appendChild(th);
+	});
+
+	studentsList.forEach((student) => {
+		const row = table.insertRow();
+		const cell1 = row.insertCell();
+		const cell2 = row.insertCell();
+		cell1.textContent = student.firstName;
+		cell2.textContent = student.location;
+	});
+
+	document.body.appendChild(table);
+}
+
+window.addEventListener('DOMContentLoaded', renderTable);
+
+
+
